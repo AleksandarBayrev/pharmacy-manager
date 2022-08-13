cf3918cc95804293f02cd0b79da80e662e842a0c
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-08-13 15:46:52 +0300

    Update logic for fetching data mocks

--- PharmacyManager.API/wwwroot/test.js
+++ PharmacyManager.API/wwwroot/test.js
@@ -1,5 +1,6 @@
-﻿(async () => {
+﻿const fetchData = async () => {
     const path = "/api/medicines/getAllMedicines";
+    document.getElementById("test").innerHTML = "";
     try {
         const result = await fetch("/api/medicines/getAllMedicines", {
             method: "POST",
@@ -7,12 +8,14 @@
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                page: 1,
-                itemsPerPage: 20
+                page: Math.floor((Math.random() * 1000)),
+                itemsPerPage: Math.floor((Math.random() * 100))
             })
         }).then(x => x.json());
         result.map(x => document.getElementById('test').innerHTML += `<div id="${x.id}">ID: ${x.id}</div><div id="${x.id}-${x.name}">${x.name}</div>`);
     } catch (err) {
         document.getElementById("test").innerHTML = `Request to ${path} failed!`;
     }
-})();
+};
+
+setInterval(fetchData, 1000);

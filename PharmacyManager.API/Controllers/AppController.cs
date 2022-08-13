95297e4cdba5e8ed20c5ef853d628197c523361a
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-08-13 15:07:15 +0300

    Add app controller on which the FE app will be rendered

---
+++ PharmacyManager.API/Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace PharmacyManager.API.Controllers
+{
+    [Route("/")]
+    [Route("/app")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        public AppController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetHTML()
+        {
+            return new FileContentResult(Encoding.UTF8.GetBytes("<html><head><title>Index</title></head><body></body></html>"), "text/html");
+        }
+    }
+}

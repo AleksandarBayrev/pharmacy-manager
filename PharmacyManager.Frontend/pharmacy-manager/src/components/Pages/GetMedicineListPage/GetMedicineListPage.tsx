d0ce7795a7344acfce9ac57b468358bc707b32c9
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-08-14 08:45:57 +0300

    Disable buttons while data is being loaded

--- PharmacyManager.Frontend/pharmacy-manager/src/components/Pages/GetMedicineListPage/GetMedicineListPage.tsx
+++ PharmacyManager.Frontend/pharmacy-manager/src/components/Pages/GetMedicineListPage/GetMedicineListPage.tsx
@@ -69,8 +69,8 @@ export class GetMedicineListPage extends React.Component<GetMedicineListPageProp
         <input type="text" onChange={(e) => this.updateRequest({ page: parseInt(e.target.value) })} placeholder={'Page'} value={this.state.request.page} />
         <input type="text" onChange={(e) => this.updateRequest({ itemsPerPage: parseInt(e.target.value) })} placeholder={'Items Per Page'} value={this.state.request.itemsPerPage} />
         <input type="text" onChange={(e) => this.updateRequest({ manufacturer: e.target.value })} placeholder={'Manufacturer'} value={this.state.request.manufacturer} />
-        <button onClick={async () => await this.getMedicines(this.state.request)}>Get medicines</button>
-        <button onClick={() => this.resetRequestToDefaults()}>Reset to default request</button>
+        <button onClick={async () => await this.getMedicines(this.state.request)} disabled={this.state.loadingData}>Get medicines</button>
+        <button onClick={() => this.resetRequestToDefaults()} disabled={this.state.loadingData}>Reset to default request</button>
         <input readOnly={true} value={`Avaliable pages: ${this.state.pages}`} />
         {this.state.loadingData ? <div className='App-data-loading'>Loading data, please wait...</div> :
           <textarea readOnly={true} value={this.state.medicines.map(x => JSON.stringify(x)).join('\n')}></textarea>}

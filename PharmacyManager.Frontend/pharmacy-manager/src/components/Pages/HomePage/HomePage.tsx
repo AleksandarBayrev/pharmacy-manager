2f51bf0c9e65c5340cf9ebcf7a4402050285b6c2
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-10-16 19:02:11 +0300

    Update home page

--- PharmacyManager.Frontend/pharmacy-manager/src/components/Pages/HomePage/HomePage.tsx
+++ PharmacyManager.Frontend/pharmacy-manager/src/components/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "../../Shared/Styles.css";
 
 export class HomePage extends React.Component {
     componentDidMount(): void {
         window.document.title = "Pharmacy Manager - Home";
     }
     render(): React.ReactNode {
-        return <>Select one of the actions from the menu above</>;
+        return (
+            <div className="App-page">
+                <div className="App-page-header">Select one of the actions from the menu above</div>
+            </div>
+        );
     }
 }

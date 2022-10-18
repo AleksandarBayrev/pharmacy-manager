1be4d891187b635f040ebf8eaf17d3a7ff1be992
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-10-18 17:14:38 +0300

    Added Date and time component

--- PharmacyManager.Frontend/pharmacy-manager/src/components/PharmacyManagerApp/App.tsx
+++ PharmacyManager.Frontend/pharmacy-manager/src/components/PharmacyManagerApp/App.tsx
@@ -5,6 +5,7 @@ import { pages } from './constants';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { DependencyInjection } from '../../base';
 import { NotFoundPage } from '../Pages/NotFoundPage/NotFoundPage';
+import { DateTime } from '../Shared/Components/DateTime';
 
 export type PharmacyManagerAppProps = {
   DependencyInjection: DependencyInjection;
@@ -18,25 +19,30 @@ export class PharmacyManagerApp extends React.Component<PharmacyManagerAppProps,
     const pageRenderer = this.props.DependencyInjection.getService<IPageRenderer>("IPageRenderer");
     return (
       <div className="App">
-      <BrowserRouter>
-        <header className="App-header">
-          <div className='App-menu'>
+        <BrowserRouter>
+          <header className="App-header">
+            <div className='App-menu'>
               <Link to={`${pages.Home}`} className='App-menu-item'>Home</Link>
               <Link to={`${pages.GetMedicinesList}`} className='App-menu-item'>Get Medicine List</Link>
               <Link to={`${pages.AddMedicines}`} className='App-menu-item'>Add Medicine</Link>
               <Link to={`${pages.UpdateMedicines}`} className='App-menu-item'>Update Medicine</Link>
-          </div>
-          <div className='App-page-container'>
-            <Routes>
-              <Route path={`${pages.Home}`} element={pageRenderer.get(pages.Home)} />
-              <Route path={`${pages.GetMedicinesList}`} element={pageRenderer.get(pages.GetMedicinesList)} />
-              <Route path={`${pages.AddMedicines}`} element={pageRenderer.get(pages.AddMedicines)} />
-              <Route path={`${pages.UpdateMedicines}`} element={pageRenderer.get(pages.UpdateMedicines)} />
-              <Route path={`*`} element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </header>
-      </BrowserRouter>
+            </div>
+            <div className='App-page-container'>
+              <div className='App-date-time'>
+                <DateTime />
+              </div>
+              <div className='App-page'>
+                <Routes>
+                  <Route path={`${pages.Home}`} element={pageRenderer.get(pages.Home)} />
+                  <Route path={`${pages.GetMedicinesList}`} element={pageRenderer.get(pages.GetMedicinesList)} />
+                  <Route path={`${pages.AddMedicines}`} element={pageRenderer.get(pages.AddMedicines)} />
+                  <Route path={`${pages.UpdateMedicines}`} element={pageRenderer.get(pages.UpdateMedicines)} />
+                  <Route path={`*`} element={<NotFoundPage />} />
+                </Routes>
+              </div>
+            </div>
+          </header>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ PharmacyManager.Frontend/pharmacy-manager/src/components/Shared/Components/DateTime.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+type DateTimeProps = {}
+
+type DateTimeState = {
+    date: Date;
+}
+
+export class DateTime extends React.Component<DateTimeProps, DateTimeState> {
+    private updateInterval: NodeJS.Timer | undefined;
+    constructor(props: DateTimeProps) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount(): void {
+        this.updateInterval = setInterval(() => {
+            this.setState({
+                date: new Date()
+            });
+        }, 50);
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.updateInterval);
+    }
+
+    private formatDate() {
+        const day = this.state.date.getDate();
+        const month = this.state.date.getMonth() + 1;
+        const year = this.state.date.getFullYear();
+
+        const dayFormatted = day < 10 ? `0${day}` : day;
+        const monthFormatted = month < 10 ? `0${month}` : month;
+        return `${dayFormatted}.${monthFormatted}.${year}`;
+    }
+
+    private formatTime() {
+        const hours = this.state.date.getHours();
+        const minutes = this.state.date.getMinutes();
+        const seconds = this.state.date.getSeconds();
+
+        const hoursFormatted = hours < 10 ? `0${hours}` : hours;
+        const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+        const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
+        return `${hoursFormatted}:${minutesFormatted}:${secondsFormatted}`;
+    }
+
+    render() {
+        return (
+            <div className="App-date-time-container">
+                {this.formatDate()} {this.formatTime()}
+            </div>
+        )
+    }
+}
